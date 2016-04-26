6faa53af67066bdd36eeb526f36e3f3ea57fe766
feiatnokia <fei.gao@gmail.com>
2016-04-26 17:10:55 +0800

    first file

--- src/test/java/CaculatorTest.java
+++ src/test/java/CaculatorTest.java
@@ -1,5 +1,15 @@
+import com.wdmx.org.Caculator;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by f2gao on 2016/4/26.
  */
 public class CaculatorTest {
+
+    @Test
+    public void add1and1() {
+        Caculator calc = new Caculator();
+        Assert.assertEquals(calc.add(1, 1), 2);
+    }
 }
