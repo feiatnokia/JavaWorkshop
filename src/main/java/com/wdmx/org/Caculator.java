c59c13912c1a3ea20defa54558f8a5ae3c9800b8
feiatnokia <fei.gao@gmail.com>
2016-04-26 17:52:34 +0800

    first file

--- src/main/java/com/wdmx/org/Caculator.java
+++ src/main/java/com/wdmx/org/Caculator.java
@@ -1,10 +1,15 @@
 package com.wdmx.org;
 
 /**
- * Created by f2gao on 2016/4/26.
+ * Created by wdmx
+ * Date: 2016/4/26.
  */
 public class Caculator {
     public int add(int i, int i1) {
         return i + i1;
+    }
+
+    public int multiple(int i, int i1) {
+        return i*i1;
     }
 }
